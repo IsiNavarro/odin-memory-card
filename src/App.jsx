845b3011072ae51e91418096187def5f9d82800d
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-09-16 13:37:40 +0200

    Display GameOn Displays deck. User makes a choice. Checks for a correct choice and updates scores accordingly. If choice was wrong gameState equals 'over' and stops displaying GameOn.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import sampleDeck from './js/Deck';
+import GameController from './js/GameController';
+
+import GameOn from './components/gameOn/GameOn';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState(sampleDeck.cards);
+  const [score, setScore] = useState(GameController.score);
+  const [bestScore, setBestScore] = useState(GameController.bestScore);
+  const [gameState, setGameState] = useState(GameController.gameState);
+
+  const handleCardClick = (e) => {
+    const choice = e.target.id;
+    GameController.playRound(choice);
+
+    setGameState(GameController.gameState);
+    setScore(GameController.score);
+    setBestScore(GameController.bestScore);
+    setCards((prevCards) => sampleDeck.shuffleCards([...prevCards]));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      {gameState === 'on' && (
+        <GameOn
+          cards={cards}
+          handleCardClick={handleCardClick}
+          score={score}
+          bestScore={bestScore}
+        />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
